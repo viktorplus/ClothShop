4ade05dd9db090abf88e41fa4e9f225b5f82210f
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-10-19 19:09:30 +0200

    point1

---
+++ Domain/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WpfApp6.Domain
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public User(string firstName, string lastName, string email, string password, DateTime dateOfBirth, string address, string phoneNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+            DateOfBirth = dateOfBirth;
+            Address = address;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}

4bdcd1f7eac8ed79d09624676314374d590e896c
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-10-19 19:09:30 +0200

    point1

---
+++ Domain/GudList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp6.Domain
+{
+    public class GudList
+    {
+        private List<Gud> goods;
+
+        public GudList()
+        {
+            goods = new List<Gud>();
+        }
+
+        public void AddGud(Gud gud)
+        {
+            goods.Add(gud);
+        }
+
+        public void RemoveGud(Gud gud)
+        {
+            goods.Remove(gud);
+        }
+
+        public List<Gud> GetAllGoods()
+        {
+            return goods;
+        }
+        public List<Gud> SearchGoodsByName(string partialName)
+        {
+            partialName = partialName.ToLower();
+
+            return goods
+                .Where(g => g.Name.ToLower().Contains(partialName))
+                .ToList();
+        }
+        public List<Gud> SortGoodsByPrice()
+        {
+            return goods.OrderBy(g => g.Price).ToList();
+        }
+
+        public List<Gud> SortGoodsByName()
+        {
+            return goods.OrderBy(g => g.Name).ToList();
+        }
+
+        public List<Gud> SortGoodsByDateAdded()
+        {
+            return goods.OrderByDescending(g => g.DateAdded).ToList();
+        }
+    }
+}

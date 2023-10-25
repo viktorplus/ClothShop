8ac38da28b986d6048d4fa248889a01b08aeb797
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-10-25 19:19:30 +0200

    v.0_25_10

---
+++ Navigator/Navigator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp6.Navigator
+{
+        //public static MainWindow? pageSwitcher;
+
+        //public static void Switch(UserControl newPage)
+        //{
+        //    pageSwitcher?.Navigate(newPage);
+        //}
+
+        //public static void Switch(UserControl newPage, object state)
+        //{
+        //    pageSwitcher?.Navigate(newPage, state);
+        //}
+}
